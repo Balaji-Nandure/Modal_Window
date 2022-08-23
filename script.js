1a36112196964f62ab06b8001fa737fb9d626832
Balaji-Nandure <balajirn.2016@gmail.com>
2022-08-23 13:00:48 +0530

    added functionality for closing window 😁

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+// console.log(btnsOpenModal);
+
+const openWindow = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
+
+const closeWindow = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+	btnsOpenModal[i].addEventListener("click", openWindow);
+}
+
+btnCloseModal.addEventListener(`click`, closeWindow);
+overlay.addEventListener(`click`, closeWindow);
+
+document.addEventListener(`keydown`, function (e) {
+	// console.log(e.key);
+
+	if (e.key === `Escape`) {
+		closeWindow();
+	}
+});
